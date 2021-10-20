b8c949a5416d1836572c244e2b46e2d187ebfe75
Amit Madan <amit.madan18@gmail.com>
2021-10-19 23:08:27 -0400

    First Publish

--- FrontEnd/src/app/property/property-list/property-list.component.ts
+++ FrontEnd/src/app/property/property-list/property-list.component.ts
@@ -7,6 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+  Properties: Array<any> = [{
+    "Id": 1,
+    "Name": "Birla House",
+    "Type": "House",
+    "Price":  12000
+  },{
+    "Id": 2,
+    "Name": "Amit House",
+    "Type": "House",
+    "Price":  10000
+  },{
+    "Id": 3,
+    "Name": "Sumit House",
+    "Type": "House",
+    "Price":  11000
+  },{
+    "Id": 4,
+    "Name": "Prateek House",
+    "Type": "House",
+    "Price":  13000
+  },{
+    "Id": 5,
+    "Name": "Vivek House",
+    "Type": "House",
+    "Price":  14000
+  },{
+    "Id": 6,
+    "Name": "Rahul House",
+    "Type": "House",
+    "Price":  15000
+  }]
+
   constructor() { }
 
   ngOnInit(): void {
